c03d34f40ca1c10f40b4c57cdd089f303acfa56c
Székely Balázs <szekelyb91@gmail.hu>
2023-01-17 18:04:41 +0100

    elkezdve

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const baseElement = document.querySelector("#base");
+const heightElement = document.querySelector("#height");
+const calcButton = document.querySelector("#calcButton");
+const areaElement = document.querySelector("#area");
+
+calcButton.addEventListener("click", () => {
+    //console.log("Működik");
+    var base = baseElement.value;
+    console.log(base);
+});
+
